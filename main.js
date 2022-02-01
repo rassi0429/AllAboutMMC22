cfb139592be0053cb0261fecaf355cf9f31350b2
rassi0429 <taitokokoa+rassi@gmail.com>
2022-02-02 04:33:17 +0900

    endpoint

--- main.js
+++ main.js
@@ -4,11 +4,19 @@ const j2e = require("json2emap")
 const app = express()
 
 const recordUrl = "https://api.neos.com/api/records/search"
-const body = { "private": false, "submittedTo": "G-Neos", "recordType": "world", "maxItems": 10, "count": 150, "requiredTags": ["mmc21"] }
+function body(tags) {
+    return { "private": false, "submittedTo": "G-Neos", "recordType": "world", "maxItems": 10, "count": 150, "requiredTags": tags }
+}
 
 
 app.get('/world/mmc21', async function (req, res) {
-    const { data } = await axios.post(recordUrl, body)
+    const { data } = await axios.post(recordUrl, body(["mmc21"]))
+    const parsed = data.map(res => { return { "worldRec": `neosrec:///${res.ownerId}/${res.id}`, "thumbnail": res.thumbnailUri, "firstPublishTime": res.firstPublishTime, tags: res.tags } })
+    res.send(req.query.json ? parsed : j2e(parsed))
+})
+
+app.get('/world/mmc22', async function (req, res) {
+    const { data } = await axios.post(recordUrl, body(["mmc22"]))
     const parsed = data.map(res => { return { "worldRec": `neosrec:///${res.ownerId}/${res.id}`, "thumbnail": res.thumbnailUri, "firstPublishTime": res.firstPublishTime, tags: res.tags } })
     res.send(req.query.json ? parsed : j2e(parsed))
 })
