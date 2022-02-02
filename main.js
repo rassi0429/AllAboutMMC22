dbf0d770c7fd8cb8859260b6e1b1c5df9778c794
rassi0429 <taitokokoa+rassi@gmail.com>
2022-02-02 13:36:18 +0900

    feat: a

--- main.js
+++ main.js
@@ -23,75 +23,56 @@ app.get('/world/mmc22', async function (req, res) {
 })
 
 app.get('/world/mmc20', async function (req, res) {
-    const { data } = await axios.post(recordUrl, {"private": false,"submittedTo": "G-Neos","recordType": "world","maxItems": 10,"count": 150,"requiredTags": ["mmc"],"maxDate":"2020-10-03T00:00:00Z"})
+    const { data } = await axios.post(recordUrl, { "private": false, "submittedTo": "G-Neos", "recordType": "world", "maxItems": 10, "count": 150, "requiredTags": ["mmc"], "maxDate": "2020-10-03T00:00:00Z" })
     const parsed = data.map(res => formatWorld(res))
     res.send(req.query.json ? parsed : j2e(parsed))
 })
 
 app.get("/a/mmc21", async (req, res) => {
-    const { data } = await axios.port(recordUrl, body(["mmc21"]))
-
-})
-
-const server = app.listen(3000, function () {
-    console.log("ok port:" + server.address().port)
-});
-
-(async () => {
-    const span = 7
+    const span = Number(req.query.span) || 7
     const { data } = await axios.post(recordUrl, body(["mmc21"]))
     const sorted = _.sortBy(data, "firstPublishTime")
     let result = []
     console.log(sorted.map(res => formatWorld(res).firstPublishTime))
+    let startDate = null
     for (let i = 31 - span; i > 0; i -= span) {
         const endDate = new Date(Date.UTC(2021, 8, i + span, 18, 0, 0))
-        const startDate = new Date(Date.UTC(2021, 8, i, 18, 0, 0))
+        startDate = new Date(Date.UTC(2021, 8, i, 18, 0, 0))
         console.log(startDate, endDate)
-        let template = {
-            world_social: [],
-            world_game: [],
-            world_misc: [],
-            avatar_avatars: [],
-            avatar_accessories: [],
-            avatar_misc: [],
-            other_tau: [],
-            other_misc: [],
-            meme: []
-        }
-        sorted.forEach(k => {
-            const firstPublishTime = new Date(k.firstPublishTime)
-            if (startDate < firstPublishTime && endDate > firstPublishTime) {
-                if (k.tags.includes("world")) {
-                    if (k.tags.includes("social")) {
-                        template.world_social.push(k)
-                    } else if (k.tags.includes("game")) {
-                        template.world_game.push(k)
-                    } else if (k.tags.includes("misc")) {
-                        template.world_misc.push(k)
-                    }
-                } else if (k.tags.includes("avatar")) {
-                    if (k.tags.includes("avatars")) {
-                        template.avatar_avatars.push(k)
-                    } else if (k.tags.includes("accessories")) {
-                        template.avatar_accessories.push(k)
-                    } else if (k.tags.includes("misc")) {
-                        template.avatar_misc.push(k)
-                    }
-                } else if (k.tags.includes("other")) {
-                    if (k.tags.includes("tau")) {
-                        template.other_tau.push(k)
-                    } else if (k.tags.includes("misc")) {
-                        template.other_misc.push(k)
-                    }
-                } else if (k.tags.includes("meme")) {
-                    template.meme.push(k)
-                }
-            }
-        })
-        result.push(template)
+        result.push(getEvent21(startDate, endDate, sorted))
     }
-    console.log(result)
-})()
+    if (startDate.getTime() != new Date(Date.UTC(2021, 8, 1, 18, 0, 0)).getTime()) {
+        result.push(getEvent21(new Date(Date.UTC(2021, 8, 1, 18, 0, 0)),startDate, sorted))
+    }
+    console.log(result.reverse())
+    res.send(req.query.json ? result.reverse() : j2e(result.reverse()))
+})
+
+
+app.get("/a/mmc20", async (req, res) => {
+    const span = Number(req.query.span) || 7
+    const { data } = await axios.post(recordUrl, body(["mmc21"]))
+    const sorted = _.sortBy(data, "firstPublishTime")
+    let result = []
+    console.log(sorted.map(res => formatWorld(res).firstPublishTime))
+    let startDate = null
+    for (let i = 31 - span; i > 0; i -= span) {
+        const endDate = new Date(Date.UTC(2021, 8, i + span, 18, 0, 0))
+        startDate = new Date(Date.UTC(2021, 8, i, 18, 0, 0))
+        console.log(startDate, endDate)
+        result.push(getEvent21(startDate, endDate, sorted))
+    }
+    if (startDate.getTime() != new Date(Date.UTC(2021, 8, 1, 18, 0, 0)).getTime()) {
+        result.push(getEvent21(new Date(Date.UTC(2021, 8, 1, 18, 0, 0)),startDate, sorted))
+    }
+    console.log(result.reverse())
+    res.send(req.query.json ? result.reverse() : j2e(result.reverse()))
+})
+
+
+const server = app.listen(3000, function () {
+    console.log("ok port:" + server.address().port)
+});
 
 function formatWorld(res) {
     return {
@@ -104,4 +85,49 @@ function formatWorld(res) {
         "name": res.name,
         "description": res.description
     }
+}
+
+function getEvent21(startDate, endDate, sorted) {
+    let template = {
+        world_social: [],
+        world_game: [],
+        world_misc: [],
+        avatar_avatars: [],
+        avatar_accessories: [],
+        avatar_misc: [],
+        other_tau: [],
+        other_misc: [],
+        meme: []
+    }
+    sorted.forEach(k => {
+        const firstPublishTime = new Date(k.firstPublishTime)
+        if (startDate < firstPublishTime && endDate > firstPublishTime) {
+            if (k.tags.includes("world") || k.tags.includes("World")) {
+                if (k.tags.includes("social") || k.tags.includes("Social")) {
+                    template.world_social.push(k)
+                } else if (k.tags.includes("game") || k.tags.includes("Game")) {
+                    template.world_game.push(k)
+                } else if (k.tags.includes("misc") || k.tags.includes("Misc")) {
+                    template.world_misc.push(k)
+                }
+            } else if (k.tags.includes("avatar") || k.tags.includes("Avatar")) {
+                if (k.tags.includes("avatars") || k.tags.includes("Avatars")) {
+                    template.avatar_avatars.push(k)
+                } else if (k.tags.includes("accessories") || k.tags.includes("Accessories")) {
+                    template.avatar_accessories.push(k)
+                } else if (k.tags.includes("misc") || k.tags.includes("Misc")) {
+                    template.avatar_misc.push(k)
+                }
+            } else if (k.tags.includes("other") || k.tags.includes("Other")) {
+                if (k.tags.includes("tau") || k.tags.includes("TAU")) {
+                    template.other_tau.push(k)
+                } else if (k.tags.includes("misc") || k.tags.includes("Misc")) {
+                    template.other_misc.push(k)
+                }
+            } else if (k.tags.includes("meme") || k.tags.includes("Meme")) {
+                template.meme.push(k)
+            }
+        }
+    })
+    return template
 }
